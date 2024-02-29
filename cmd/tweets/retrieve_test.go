9ca37fa7abc967d1b7548b49b47838354beb7f66
Lucas <lhbelfanti@gmail.com>
2024-02-29 20:00:42 -0300

    Added missing unit tests for retrieve.go file

--- cmd/tweets/retrieve_test.go
+++ cmd/tweets/retrieve_test.go
@@ -1,6 +1,7 @@
 package tweets_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,8 +13,7 @@ import (
 
 func TestRetrieveAll_success(t *testing.T) {
 	mockWebElement := new(elements.MockWebElement)
-	mockWebElements := []selenium.WebElement{mockWebElement, mockWebElement}
-	mockRetrieveAll := elements.MockWaitAndRetrieveAll(mockWebElements, nil)
+	mockRetrieveAll := elements.MockWaitAndRetrieveAll([]selenium.WebElement{mockWebElement, mockWebElement}, nil)
 	mockTweet := tweets.MockTweet()
 	mockGatherTweetInformation := tweets.MockGatherTweetInformation(mockTweet, nil)
 
@@ -24,4 +24,31 @@ func TestRetrieveAll_success(t *testing.T) {
 
 	assert.Equal(t, want, got)
 	assert.Nil(t, err)
+}
+
+func TestRetrieveAll_successEvenWhenGatherTweetInformationThrowsError(t *testing.T) {
+	mockWebElement := new(elements.MockWebElement)
+	mockRetrieveAll := elements.MockWaitAndRetrieveAll([]selenium.WebElement{mockWebElement, mockWebElement}, nil)
+	mockGatherTweetInformation := tweets.MockGatherTweetInformation(tweets.MockTweet(), errors.New("error"))
+
+	retrieveAll := tweets.MakeRetrieveAll(mockRetrieveAll, mockGatherTweetInformation)
+
+	var want []tweets.Tweet
+	got, err := retrieveAll()
+
+	assert.Equal(t, want, got)
+	assert.Nil(t, err)
+}
+
+func TestRetrieveAll_failsWhenWaitAndRetrieveElementThrowsError(t *testing.T) {
+	mockWebElement := new(elements.MockWebElement)
+	mockRetrieveAll := elements.MockWaitAndRetrieveAll([]selenium.WebElement{mockWebElement, mockWebElement}, errors.New("error"))
+	mockGatherTweetInformation := tweets.MockGatherTweetInformation(tweets.MockTweet(), nil)
+
+	retrieveAll := tweets.MakeRetrieveAll(mockRetrieveAll, mockGatherTweetInformation)
+
+	want := tweets.FailedToRetrieveArticles
+	_, got := retrieveAll()
+
+	assert.Equal(t, want, got)
 }
