353cfdc4f0fb249a914e533e559715c55abc1615
Lucas <lhbelfanti@gmail.com>
2024-11-25 00:54:20 -0300

    ⚡️ Implemented GetQuoteAuthor function

--- cmd/api/tweets/author.go
+++ cmd/api/tweets/author.go
@@ -8,15 +8,25 @@ import (
 	"goxcrap/internal/log"
 )
 
-const authorXPath string = "div[1]/div/div[1]/div/div/div[2]/div/div[1]/a/div/span"
+const (
+	tweetAuthorXPath string = "div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[1]/a/div/span"
 
-// GetAuthor retrieves the tweet author
-type GetAuthor func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error)
+	quoteAuthorTweetHasOnlyTextXPath     = "div[2]/div[3]/div/div[2]/div/div[1]/div/div/div/div[2]/div/div[2]/div/div[1]/div/div/span"
+	quoteAuthorTweetHasTextAndImageXPath = "div[2]/div[3]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/div/div[2]/div/div[1]/div/div/span"
+)
+
+type (
+	// GetAuthor retrieves the tweet author
+	GetAuthor func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error)
+
+	// GetQuoteAuthor retrieves the quoted tweet author
+	GetQuoteAuthor func(ctx context.Context, tweetArticleElement selenium.WebElement, hasTweetOnlyText bool) (string, error)
+)
 
 // MakeGetAuthor creates a new GetAuthor
 func MakeGetAuthor() GetAuthor {
 	return func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error) {
-		tweetAuthorElement, err := tweetArticleElement.FindElement(selenium.ByXPATH, globalToLocalXPath(authorXPath))
+		tweetAuthorElement, err := tweetArticleElement.FindElement(selenium.ByXPATH, globalToLocalXPath(tweetAuthorXPath))
 		if err != nil {
 			log.Error(ctx, err.Error())
 			return "", FailedToObtainTweetAuthorElement
@@ -29,5 +39,29 @@ func MakeGetAuthor() GetAuthor {
 		}
 
 		return tweetAuthor, nil
+	}
+}
+
+// MakeGetQuoteAuthor creates a new GetQuoteAuthor
+func MakeGetQuoteAuthor() GetQuoteAuthor {
+	return func(ctx context.Context, tweetArticleElement selenium.WebElement, hasTweetOnlyText bool) (string, error) {
+		xPath := quoteAuthorTweetHasTextAndImageXPath
+		if hasTweetOnlyText {
+			xPath = quoteAuthorTweetHasOnlyTextXPath
+		}
+
+		quoteAuthorElement, err := tweetArticleElement.FindElement(selenium.ByXPATH, globalToLocalXPath(xPath))
+		if err != nil {
+			log.Error(ctx, err.Error())
+			return "", FailedToObtainQuotedTweetAuthorElement
+		}
+
+		quoteAuthor, err := quoteAuthorElement.Text()
+		if err != nil {
+			log.Error(ctx, err.Error())
+			return "", FailedToObtainQuotedTweetAuthor
+		}
+
+		return quoteAuthor, nil
 	}
 }
--- cmd/api/tweets/author_test.go
+++ cmd/api/tweets/author_test.go
@@ -58,3 +58,70 @@ func TestGetAuthor_failsWhenTextThrowsError(t *testing.T) {
 	mockTweetArticleWebElement.AssertExpectations(t)
 	mockTweetAuthorWebElement.AssertExpectations(t)
 }
+
+func TestGetQuoteAuthor_success(t *testing.T) {
+	for _, test := range []struct {
+		hasTweetOnlyText bool
+	}{
+		{hasTweetOnlyText: false},
+		{hasTweetOnlyText: true},
+	} {
+		mockTweetArticleWebElement := new(elements.MockWebElement)
+		mockQuoteAuthorWebElement := new(elements.MockWebElement)
+		mockTweetArticleWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockQuoteAuthorWebElement), nil)
+		mockQuoteAuthorWebElement.On("Text", mock.Anything).Return("test", nil)
+
+		getQuoteAuthor := tweets.MakeGetQuoteAuthor()
+
+		want := "test"
+		got, err := getQuoteAuthor(context.Background(), mockTweetArticleWebElement, test.hasTweetOnlyText)
+
+		assert.Equal(t, want, got)
+		assert.Nil(t, err)
+		mockTweetArticleWebElement.AssertExpectations(t)
+		mockQuoteAuthorWebElement.AssertExpectations(t)
+	}
+}
+
+func TestGetQuoteAuthor_failsWhenFindElementThrowsError(t *testing.T) {
+	for _, test := range []struct {
+		hasTweetOnlyText bool
+	}{
+		{hasTweetOnlyText: false},
+		{hasTweetOnlyText: true},
+	} {
+		mockTweetArticleWebElement := new(elements.MockWebElement)
+		mockTweetArticleWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(new(elements.MockWebElement)), errors.New("error while executing FindElement"))
+
+		getQuoteAuthor := tweets.MakeGetQuoteAuthor()
+
+		want := tweets.FailedToObtainQuotedTweetAuthorElement
+		_, got := getQuoteAuthor(context.Background(), mockTweetArticleWebElement, test.hasTweetOnlyText)
+
+		assert.Equal(t, want, got)
+		mockTweetArticleWebElement.AssertExpectations(t)
+	}
+}
+
+func TestGetQuoteAuthor_failsWhenTextThrowsError(t *testing.T) {
+	for _, test := range []struct {
+		hasTweetOnlyText bool
+	}{
+		{hasTweetOnlyText: false},
+		{hasTweetOnlyText: true},
+	} {
+		mockTweetArticleWebElement := new(elements.MockWebElement)
+		mockQuoteAuthorWebElement := new(elements.MockWebElement)
+		mockTweetArticleWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockQuoteAuthorWebElement), nil)
+		mockQuoteAuthorWebElement.On("Text", mock.Anything).Return("", errors.New("error while executing Text"))
+
+		getQuoteAuthor := tweets.MakeGetQuoteAuthor()
+
+		want := tweets.FailedToObtainQuotedTweetAuthor
+		_, got := getQuoteAuthor(context.Background(), mockTweetArticleWebElement, test.hasTweetOnlyText)
+
+		assert.Equal(t, want, got)
+		mockTweetArticleWebElement.AssertExpectations(t)
+		mockQuoteAuthorWebElement.AssertExpectations(t)
+	}
+}
