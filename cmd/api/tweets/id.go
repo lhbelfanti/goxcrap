fe7ddcfb1c63b52931f9bfe86c8d91585f967e71
Lucas Belfanti <lhbelfanti@gmail.com>
2024-12-27 16:23:52 -0300

    ⚡️ Added GetIDFromTweetPage function

--- cmd/api/tweets/id.go
+++ cmd/api/tweets/id.go
@@ -9,9 +9,20 @@ import (
 	"goxcrap/internal/log"
 )
 
-const tweetIDXPath string = "div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[3]"
+const (
+	tweetIDXPath string = "div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[3]"
 
-type GetID func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error)
+	tweetIDElementFromHeaderXPath string = "div[2]/div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[3]"
+	tweetIDElementFromFooterXPath string = "div[3]/div[4]/div/div[1]/div/div[1]"
+)
+
+type (
+	// GetID retrieves the ID of the tweet
+	GetID func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error)
+
+	// GetIDFromTweetPage retrieves the ID of a tweet from the opened tweet page
+	GetIDFromTweetPage func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error)
+)
 
 // MakeGetID creates a new GetID
 func MakeGetID() GetID {
@@ -22,21 +33,46 @@ func MakeGetID() GetID {
 			return "", FailedToObtainTweetIDElement
 		}
 
-		tweetIDATag, err := tweetIDElement.FindElement(selenium.ByTagName, "a")
-		if err != nil {
-			log.Warn(ctx, err.Error())
-			return "", FailedToObtainTweetIDATag
-		}
-
-		tweetIDHref, err := tweetIDATag.GetAttribute("href")
-		if err != nil {
-			log.Warn(ctx, err.Error())
-			return "", FailedToObtainTweetIDATagHref
-		}
-
-		hrefSplit := strings.Split(tweetIDHref, "/")
-		tweetID := hrefSplit[len(hrefSplit)-1]
-
-		return tweetID, nil
+		return obtainTweetIDFromTweet(ctx, tweetIDElement)
 	}
+}
+
+// MakeGetIDFromTweetPage creates a new GetIDFromTweetPage
+func MakeGetIDFromTweetPage() GetIDFromTweetPage {
+	return func(ctx context.Context, tweetArticleElement selenium.WebElement) (string, error) {
+		tweetIDElement, err := tweetArticleElement.FindElement(selenium.ByXPATH, globalToLocalXPath(tweetIDElementFromHeaderXPath))
+		if err != nil {
+			log.Warn(ctx, err.Error())
+			log.Debug(ctx, FailedToObtainTweetIDElementFromTweetPageHeader.Error())
+
+			tweetIDElement, err = tweetArticleElement.FindElement(selenium.ByXPATH, globalToLocalXPath(tweetIDElementFromFooterXPath))
+			if err != nil {
+				log.Warn(ctx, err.Error())
+				log.Debug(ctx, FailedToObtainTweetIDElementFromTweetPageFooter.Error())
+				return "", FailedToObtainTweetIDElementFromTweetPage
+			}
+		}
+
+		return obtainTweetIDFromTweet(ctx, tweetIDElement)
+	}
+}
+
+// obtainTweetIDFromTweet retrieves the tweet id from the given tweet element
+func obtainTweetIDFromTweet(ctx context.Context, tweetIDElement selenium.WebElement) (string, error) {
+	tweetIDATag, err := tweetIDElement.FindElement(selenium.ByTagName, "a")
+	if err != nil {
+		log.Warn(ctx, err.Error())
+		return "", FailedToObtainTweetIDATag
+	}
+
+	tweetIDHref, err := tweetIDATag.GetAttribute("href")
+	if err != nil {
+		log.Warn(ctx, err.Error())
+		return "", FailedToObtainTweetIDATagHref
+	}
+
+	hrefSplit := strings.Split(tweetIDHref, "/")
+	tweetID := hrefSplit[len(hrefSplit)-1]
+
+	return tweetID, nil
 }
--- cmd/api/tweets/id_test.go
+++ cmd/api/tweets/id_test.go
@@ -13,13 +13,18 @@ import (
 	"goxcrap/cmd/api/tweets"
 )
 
+const (
+	tweetIDElementFromHeaderXPath string = "div[position()=2]/div[position()=2]/div[position()=1]/div/div[position()=1]/div/div/div[position()=2]/div/div[position()=3]"
+	tweetIDElementFromFooterXPath string = "div[position()=3]/div[position()=4]/div/div[position()=1]/div/div[position()=1]"
+)
+
 func TestGetID_success(t *testing.T) {
 	mockTweetArticleWebElement := new(elements.MockWebElement)
 	mockTweetIDWebElement := new(elements.MockWebElement)
 	mockTweetIDATagWebElement := new(elements.MockWebElement)
 	mockTweetArticleWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockTweetIDWebElement), nil)
 	mockTweetIDWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockTweetIDATagWebElement), nil)
-	mockTweetIDATagWebElement.On("GetAttribute", mock.Anything).Return("id/123456789", nil)
+	mockTweetIDATagWebElement.On("GetAttribute", mock.Anything).Return("https://x.com/user/status/123456789", nil)
 
 	getID := tweets.MakeGetID()
 
@@ -74,6 +79,95 @@ func TestGetID_failsWhenGetAttributeThrowsError(t *testing.T) {
 
 	want := tweets.FailedToObtainTweetIDATagHref
 	_, got := getID(context.Background(), mockTweetArticleWebElement)
+
+	assert.Equal(t, want, got)
+	mockTweetArticleWebElement.AssertExpectations(t)
+	mockTweetIDWebElement.AssertExpectations(t)
+	mockTweetIDATagWebElement.AssertExpectations(t)
+}
+
+func TestGetIDFromTweetPage_success(t *testing.T) {
+	mockTweetArticleWebElement := new(elements.MockWebElement)
+	mockTweetIDFromHeaderWebElement := new(elements.MockWebElement)
+	mockTweetIDATagWebElement := new(elements.MockWebElement)
+	mockTweetArticleWebElement.On("FindElement", selenium.ByXPATH, tweetIDElementFromHeaderXPath).Return(selenium.WebElement(mockTweetIDFromHeaderWebElement), nil)
+	mockTweetIDFromHeaderWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockTweetIDATagWebElement), nil)
+	mockTweetIDATagWebElement.On("GetAttribute", mock.Anything).Return("https://x.com/user/status/123456789", nil)
+
+	getIDFromTweetPage := tweets.MakeGetIDFromTweetPage()
+
+	want := "123456789"
+	got, err := getIDFromTweetPage(context.Background(), mockTweetArticleWebElement)
+
+	assert.Equal(t, want, got)
+	assert.Nil(t, err)
+	mockTweetArticleWebElement.AssertExpectations(t)
+	mockTweetIDFromHeaderWebElement.AssertExpectations(t)
+	mockTweetIDATagWebElement.AssertExpectations(t)
+}
+
+func TestGetIDFromTweetPage_successEvenWhenFirstFindElementThrowsError(t *testing.T) {
+	mockTweetArticleWebElement := new(elements.MockWebElement)
+	mockTweetIDFromFooterWebElement := new(elements.MockWebElement)
+	mockTweetIDATagWebElement := new(elements.MockWebElement)
+	mockTweetArticleWebElement.On("FindElement", selenium.ByXPATH, tweetIDElementFromHeaderXPath).Return(selenium.WebElement(new(elements.MockWebElement)), errors.New("error while executing FindElement"))
+	mockTweetArticleWebElement.On("FindElement", selenium.ByXPATH, tweetIDElementFromFooterXPath).Return(selenium.WebElement(mockTweetIDFromFooterWebElement), nil)
+	mockTweetIDATagWebElement.On("GetAttribute", mock.Anything).Return("https://x.com/user/status/123456789", nil)
+
+	getIDFromTweetPage := tweets.MakeGetIDFromTweetPage()
+
+	want := "123456789"
+	got, err := getIDFromTweetPage(context.Background(), mockTweetArticleWebElement)
+
+	assert.Equal(t, want, got)
+	assert.Nil(t, err)
+	mockTweetArticleWebElement.AssertExpectations(t)
+	mockTweetIDFromFooterWebElement.AssertExpectations(t)
+	mockTweetIDATagWebElement.AssertExpectations(t)
+}
+
+func TestGetIDFromTweetPage_failsWhenFirstAndSecondFindElementThrowError(t *testing.T) {
+	mockTweetArticleWebElement := new(elements.MockWebElement)
+	mockTweetArticleWebElement.On("FindElement", selenium.ByXPATH, tweetIDElementFromHeaderXPath).Return(selenium.WebElement(new(elements.MockWebElement)), errors.New("error while executing FindElement"))
+	mockTweetArticleWebElement.On("FindElement", selenium.ByXPATH, tweetIDElementFromFooterXPath).Return(selenium.WebElement(new(elements.MockWebElement)), errors.New("error while executing FindElement"))
+
+	getIDFromTweetPage := tweets.MakeGetIDFromTweetPage()
+
+	want := tweets.FailedToObtainTweetIDElementFromTweetPage
+	_, got := getIDFromTweetPage(context.Background(), mockTweetArticleWebElement)
+
+	assert.Equal(t, want, got)
+	mockTweetArticleWebElement.AssertExpectations(t)
+}
+
+func TestGetIDFromTweetPage_failsWhenThirdFindElementThrowsError(t *testing.T) {
+	mockTweetArticleWebElement := new(elements.MockWebElement)
+	mockTweetIDWebElement := new(elements.MockWebElement)
+	mockTweetArticleWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockTweetIDWebElement), nil)
+	mockTweetIDWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(new(elements.MockWebElement)), errors.New("error while executing FindElement"))
+
+	getIDFromTweetPage := tweets.MakeGetIDFromTweetPage()
+
+	want := tweets.FailedToObtainTweetIDATag
+	_, got := getIDFromTweetPage(context.Background(), mockTweetArticleWebElement)
+
+	assert.Equal(t, want, got)
+	mockTweetArticleWebElement.AssertExpectations(t)
+	mockTweetIDWebElement.AssertExpectations(t)
+}
+
+func TestGetIDFromTweetPage_failsWhenGetAttributeThrowsError(t *testing.T) {
+	mockTweetArticleWebElement := new(elements.MockWebElement)
+	mockTweetIDWebElement := new(elements.MockWebElement)
+	mockTweetIDATagWebElement := new(elements.MockWebElement)
+	mockTweetArticleWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockTweetIDWebElement), nil)
+	mockTweetIDWebElement.On("FindElement", mock.Anything, mock.Anything).Return(selenium.WebElement(mockTweetIDATagWebElement), nil)
+	mockTweetIDATagWebElement.On("GetAttribute", mock.Anything).Return("", errors.New("error while executing GetAttribute"))
+
+	getIDFromTweetPage := tweets.MakeGetIDFromTweetPage()
+
+	want := tweets.FailedToObtainTweetIDATagHref
+	_, got := getIDFromTweetPage(context.Background(), mockTweetArticleWebElement)
 
 	assert.Equal(t, want, got)
 	mockTweetArticleWebElement.AssertExpectations(t)
